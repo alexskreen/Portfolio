0897999c0c664140acd59f2fa02066a3425bc85c
Alex Skreen <skreenalex@gmail.com>
2020-07-30 11:34:30 -0700

    create skills component

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl'
 import Education from './education';
+import Experience from './experience'
+import Skills from './skills'
 
 
 class Resume extends Component {
@@ -38,6 +40,34 @@ class Resume extends Component {
             <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
           </Cell>
           <Cell className="resume-right-col" col={8}>
+            <h2>Experience</h2>
+
+            <Experience
+              startYear={2019}
+              endYear={2020}
+              companyName="Deltek"
+              companyPosition="Account Executive"
+              companyDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+            />
+
+            <Experience
+              startYear={2014}
+              endYear={2019}
+              companyName="ShopKeep"
+              companyPosition="Senior Account Executive"
+              companyDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+            />
+
+            <Experience
+              startYear={2013}
+              endYear={2014}
+              companyName="Enterprise Rent-A-Car"
+              companyPosition="Assistant Manager"
+              companyDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+            />
+
+            {/* Change the color of this hr to something blueish */}
+            <hr style={{ borderTop: "3px solid #e22947" }} />
             <h2>Education</h2>
 
             <Education
@@ -53,6 +83,14 @@ class Resume extends Component {
               schoolName="Oregon State University"
               schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
             />
+
+            <hr style={{ borderTop: "3px solid #e22947" }} />
+            <h2>Skills</h2>
+            <Skills skill="javascript" progress={75} />
+            <Skills skill="HTML" progress={90} />
+            <Skills skill="C#" progress={70} />
+            <Skills skill="React" progress={50} />
+            <Skills skill="Unity" progress={60} />
           </Cell>
         </Grid>
       </div>
